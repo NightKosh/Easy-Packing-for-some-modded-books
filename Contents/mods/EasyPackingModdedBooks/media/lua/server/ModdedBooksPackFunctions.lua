002cc6b98e29d79e0c7f07855cb5a676a99b6909
NightKosh <nightkosh@gmail.com>
2022-09-02 17:33:51 +0300

    Unpack lua scripts

---
+++ Contents/mods/EasyPackingModdedBooks/media/lua/server/ModdedBooksPackFunctions.lua
@@ -0,0 +1,116 @@
+
+function Recipe.OnCreate.UnpackFitnessSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookFitness2");
+	player:getInventory():AddItem("AllSkillBooks.BookFitness3");
+	player:getInventory():AddItem("AllSkillBooks.BookFitness4");
+	player:getInventory():AddItem("AllSkillBooks.BookFitness5");
+end
+
+function Recipe.OnCreate.UnpackStrengthSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookStrength2");
+	player:getInventory():AddItem("AllSkillBooks.BookStrength3");
+	player:getInventory():AddItem("AllSkillBooks.BookStrength4");
+	player:getInventory():AddItem("AllSkillBooks.BookStrength5");
+end
+
+
+function Recipe.OnCreate.UnpackSprintingSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookSprinting2");
+	player:getInventory():AddItem("AllSkillBooks.BookSprinting3");
+	player:getInventory():AddItem("AllSkillBooks.BookSprinting4");
+	player:getInventory():AddItem("AllSkillBooks.BookSprinting5");
+end
+
+function Recipe.OnCreate.UnpackLightfootedSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookLightfooted2");
+	player:getInventory():AddItem("AllSkillBooks.BookLightfooted3");
+	player:getInventory():AddItem("AllSkillBooks.BookLightfooted4");
+	player:getInventory():AddItem("AllSkillBooks.BookLightfooted5");
+end
+
+function Recipe.OnCreate.UnpackNimbleSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookNimble2");
+	player:getInventory():AddItem("AllSkillBooks.BookNimble3");
+	player:getInventory():AddItem("AllSkillBooks.BookNimble4");
+	player:getInventory():AddItem("AllSkillBooks.BookNimble5");
+end
+
+function Recipe.OnCreate.UnpackSneakingSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookSneaking2");
+	player:getInventory():AddItem("AllSkillBooks.BookSneaking3");
+	player:getInventory():AddItem("AllSkillBooks.BookSneaking4");
+	player:getInventory():AddItem("AllSkillBooks.BookSneaking5");
+end
+
+
+function Recipe.OnCreate.UnpackAxeSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookAxe2");
+	player:getInventory():AddItem("AllSkillBooks.BookAxe3");
+	player:getInventory():AddItem("AllSkillBooks.BookAxe4");
+	player:getInventory():AddItem("AllSkillBooks.BookAxe5");
+end
+
+function Recipe.OnCreate.UnpackLongBluntSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookBlunt2");
+	player:getInventory():AddItem("AllSkillBooks.BookBlunt3");
+	player:getInventory():AddItem("AllSkillBooks.BookBlunt4");
+	player:getInventory():AddItem("AllSkillBooks.BookBlunt5");
+end
+
+function Recipe.OnCreate.UnpackShortBluntSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlunt2");
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlunt3");
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlunt4");
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlunt5");
+end
+
+function Recipe.OnCreate.UnpackLongBladeSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookLongBlade2");
+	player:getInventory():AddItem("AllSkillBooks.BookLongBlade3");
+	player:getInventory():AddItem("AllSkillBooks.BookLongBlade4");
+	player:getInventory():AddItem("AllSkillBooks.BookLongBlade5");
+end
+
+function Recipe.OnCreate.UnpackSmallBladeSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlade2");
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlade3");
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlade4");
+	player:getInventory():AddItem("AllSkillBooks.BookSmallBlade5");
+end
+
+function Recipe.OnCreate.UnpackSpearSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookSpear2");
+	player:getInventory():AddItem("AllSkillBooks.BookSpear3");
+	player:getInventory():AddItem("AllSkillBooks.BookSpear4");
+	player:getInventory():AddItem("AllSkillBooks.BookSpear5");
+end
+
+function Recipe.OnCreate.UnpackMaintenanceSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookMaintenance2");
+	player:getInventory():AddItem("AllSkillBooks.BookMaintenance3");
+	player:getInventory():AddItem("AllSkillBooks.BookMaintenance4");
+	player:getInventory():AddItem("AllSkillBooks.BookMaintenance5");
+end
+
+
+function Recipe.OnCreate.UnpackAimingSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookAiming2");
+	player:getInventory():AddItem("AllSkillBooks.BookAiming3");
+	player:getInventory():AddItem("AllSkillBooks.BookAiming4");
+	player:getInventory():AddItem("AllSkillBooks.BookAiming5");
+end
+
+function Recipe.OnCreate.UnpackReloadingSkillBook(items, result, player)
+	player:getInventory():AddItem("AllSkillBooks.BookReloading2");
+	player:getInventory():AddItem("AllSkillBooks.BookReloading3");
+	player:getInventory():AddItem("AllSkillBooks.BookReloading4");
+	player:getInventory():AddItem("AllSkillBooks.BookReloading5");
+end
+
+
+function Recipe.OnCreate.UnpackDrivingSkillBook(items, result, player)
+	player:getInventory():AddItem("DrivingSkill.DrivingSkill_BookDriving2");
+	player:getInventory():AddItem("DrivingSkill.DrivingSkill_BookDriving3");
+	player:getInventory():AddItem("DrivingSkill.DrivingSkill_BookDriving4");
+	player:getInventory():AddItem("DrivingSkill.DrivingSkill_BookDriving5");
+end
